4288e73296d3cbcd88b6dee67c87a55cd65d92a1
Vladislav Petrov <electronicchest@gmail.com>
2016-11-20 00:25:22 +0300

    Correct travis CI database configuration

--- config/travis.exs
+++ config/travis.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :phoenix_trello, PhoenixTrello.Endpoint,
+config :hotels, Hotels.Endpoint,
   http: [port: 4001],
   server: false
 
@@ -10,10 +10,10 @@ config :phoenix_trello, PhoenixTrello.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
-config :phoenix_trello, PhoenixTrello.Repo,
+config :hotels, Hotels.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "",
-  database: "phoenix_trello_test",
+  database: "hotels_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
